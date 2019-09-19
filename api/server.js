303204681f2f153d6ca81e3cd559cfbb2cb667a0
hsrsbd <hsraffety@gmail.com>
2019-09-18 18:36:01 -0700

    Router up and serving recipes

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const server = express();
-
-
+// const recipeRouter = require('../recipes/recipe-router');
 server.use(express.json());
 
-server.get('/',  (req,res) =>{
-    res.send('hello, your server is running')
-})
+server.get('/', (req, res) => {
+  res.send('hello, your server is running');
+});
 
+server.use('/recipes', (req, res) => {
+  res.send('hello from recipe route');
+});
 
 module.exports = server;
